37a345b9fece4b9ef9aa31779b962b6a9a965f96
Chihiro Hayashi <chayashi@cmc.ss.is.nagoya-u.ac.jp>
2016-04-15 11:10:56 +0900

    test

--- hoppin/GameSystem/Judgement.cs
+++ hoppin/GameSystem/Judgement.cs
@@ -8,6 +8,14 @@ namespace hoppin.GameSystem
 {
     abstract class Judgement
     {
+        //各プレイヤーの1マス移動毎に呼び出される関数
+        //やること
+        //・マスの塗り替え、囲みによる塗り替え判定
+        //・アイテム取得判定
+        //  ・アイテムの生成
+        //  ・スコア換算
+        //GameStateを返す
         abstract public void JudgePlayerMove();
+
     }
 }
