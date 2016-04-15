52a8de9ba8fe92ba25380f0e7c2d5eb41ee73ba6
MthBridge <lion.no.hito1014@gmail.com>
2016-04-15 20:42:56 +0900

    gameManager製作中

--- hoppin/GameSystem/AbstractPlayer.cs
+++ hoppin/GameSystem/AbstractPlayer.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using hoppin.GameSystem;
+
 namespace hoppin
 {
     abstract class AbstractPlayer
     {
+        private String name;
         abstract public PlayerMove move(); 
+
+        public String Name
+        {
+            get { return this.name; }
+        }
     }
 }
--- hoppin/GameSystem/GameManager.cs
+++ hoppin/GameSystem/GameManager.cs
@@ -10,7 +10,19 @@ namespace hoppin.GameSystem
     {
         private class PrivateGameState : GameState
         {
-            public PrivateGameState(AbstractPlayer player1, AbstractPlayer player2, AbstractPlayer pl)
+            public PrivateGameState(AbstractPlayer player1, AbstractPlayer player2, AbstractPlayer player3,AbstractPlayer player4)
+            {
+                playerList.Add((int)FieldId.PLAYER1, player1);
+                playerList.Add((int)FieldId.PLAYER2, player2);
+                playerList.Add((int)FieldId.PLAYER3, player3);
+                playerList.Add((int)FieldId.PLAYER4, player4);
+
+                playerScoreList.Add((int)FieldId.PLAYER1, 0);
+                playerScoreList.Add((int)FieldId.PLAYER2, 0);
+                playerScoreList.Add((int)FieldId.PLAYER3, 0);
+                playerScoreList.Add((int)FieldId.PLAYER4, 0);
+
+            }
         }
     }
 }
