0616ca95f60bc577fa5075aa40ffd266d950a539
Chihiro Hayashi <chayashi@cmc.ss.is.nagoya-u.ac.jp>
2016-04-20 23:14:03 +0900

    no message

--- hoppin/GameSystem/Judgement.cs
+++ hoppin/GameSystem/Judgement.cs
@@ -8,6 +8,35 @@ namespace hoppin.GameSystem
 {
     abstract class Judgement
     {
+        //もらう
+        //GameState
+        //  currentPlayer and currentOlayerMove
+        //playerScore
+
+        //やること
+        //1.移動後の盤面の反映
+        //  変更の要因
+        //    ・プレイヤーの移動
+        //    ・アイテムの取得
+        //      ・盤面の繁栄
+        //      ・プレイヤースコア書き換え
+        //    ・色囲みによる塗りつぶし
+        //2.アイテムの生成
         abstract public void JudgePlayerMove();
+
+        private void MovePlayer(){//プレイヤーの移動
+        
+        }
+        private void IsGetItems(){//アイテム取得
+        
+
+        }
+        private void IsBlockadeTerritory(){//囲み領域の探索
+
+        }
+        private void MakeItems(){//アイテムの生成
+
+
+        }
     }
 }
