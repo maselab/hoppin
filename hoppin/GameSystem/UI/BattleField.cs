db01782ec4116957b71d9cec9efc98b43b50033d
Ryosuke Onose <dev@onose.ws>
2016-04-24 19:50:44 +0900

    ゲームボード上のブロック描画メソッドを追加・完成

--- hoppin/GameSystem/UI/BattleField.cs
+++ hoppin/GameSystem/UI/BattleField.cs
@@ -10,6 +10,18 @@ namespace hoppin.GameSystem.UI
 {
     class BattleField
     {
+        int[,] testFieldNum = new int[,]
+        {
+            {0, 0, 0, 0, 3, 3, 3, 3 },
+            {0, 6, 7, 7, 7, 7, 7, 3 },
+            {0, 6, 0, 4, 4, 3, 5, 3 },
+            {0, 6, 4, 9, 9, 4, 5, 3 },
+            {1, 6, 4, 9, 9, 4, 5, 2 },
+            {1, 6, 1, 4, 4, 2, 5, 2 },
+            {1, 8, 8, 8, 8, 8, 5, 2 },
+            {1, 1, 1, 1, 2, 2, 2, 2 },
+        };
+
         Style style = new Style();
         FieldBlock blankBlock = new FieldBlock();
         public void drawBlankField(PaintEventArgs e) {
@@ -18,7 +30,7 @@ namespace hoppin.GameSystem.UI
             {
                 for (int j = 0; j < 8; j++)
                 {
-                    blankBlock.draw(e, j, i);
+                    blankBlock.draw(e, j, i, (BlockType)testFieldNum[i, j]);
                 }
             }
             e.Graphics.ResetTransform();
--- hoppin/GameSystem/UI/ScoreBoard.cs
+++ hoppin/GameSystem/UI/ScoreBoard.cs
@@ -15,13 +15,13 @@ namespace hoppin.GameSystem.UI
         {
             ScorePanel scorePanel = new ScorePanel();
             e.Graphics.TranslateTransform(420, 43);
-            scorePanel.draw(e, style.player1Color);
+            scorePanel.draw(e, style.playerAColor);
             e.Graphics.TranslateTransform(0, 84);
-            scorePanel.draw(e, style.player2Color);
+            scorePanel.draw(e, style.playerBColor);
             e.Graphics.TranslateTransform(0, 84);
-            scorePanel.draw(e, style.player3Color);
+            scorePanel.draw(e, style.playerCColor);
             e.Graphics.TranslateTransform(0, 84);
-            scorePanel.draw(e, style.player4Color);
+            scorePanel.draw(e, style.playerDColor);
             e.Graphics.ResetTransform();
         }
     }
