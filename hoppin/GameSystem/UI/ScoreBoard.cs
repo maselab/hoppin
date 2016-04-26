83c82d5d8ef396f7abbc0ac52c231fb78e2a9557
Ryosuke Onose <dev@onose.ws>
2016-04-27 02:25:17 +0900

    ScoreBoard 完成

--- hoppin/GameSystem/UI/ScoreBoard.cs
+++ hoppin/GameSystem/UI/ScoreBoard.cs
@@ -20,18 +20,19 @@ namespace hoppin.GameSystem.UI
         {
             List<int> playerScores = gameState.GetPlayerScore();
             List<string> playerNames = gameState.playerName;
+            
 
             e.Graphics.TranslateTransform(420, 43);
-            ScorePanel scorePanelA = new ScorePanel("A", style.playerAColor, playerNames[0], playerScores[0], true);
+            ScorePanel scorePanelA = new ScorePanel("A", style.playerAColor, playerNames[0], playerScores[0], (gameState.CurrentPlayer == FieldObject.PLAYER1) ? true : false);
             scorePanelA.draw(e);
             e.Graphics.TranslateTransform(0, 84);
-            ScorePanel scorePanelB = new ScorePanel("B", style.playerBColor, playerNames[1], playerScores[1], false);
+            ScorePanel scorePanelB = new ScorePanel("B", style.playerBColor, playerNames[1], playerScores[1], (gameState.CurrentPlayer == FieldObject.PLAYER2) ? true : false);
             scorePanelB.draw(e);
             e.Graphics.TranslateTransform(0, 84);
-            ScorePanel scorePanelC = new ScorePanel("C", style.playerCColor, playerNames[2], playerScores[2], false);
+            ScorePanel scorePanelC = new ScorePanel("C", style.playerCColor, playerNames[2], playerScores[2], (gameState.CurrentPlayer == FieldObject.PLAYER3) ? true : false);
             scorePanelC.draw(e);
             e.Graphics.TranslateTransform(0, 84);
-            ScorePanel scorePanelD = new ScorePanel("D", style.playerDColor, playerNames[3], playerScores[3], false);
+            ScorePanel scorePanelD = new ScorePanel("D", style.playerDColor, playerNames[3], playerScores[3], (gameState.CurrentPlayer == FieldObject.PLAYER4) ? true : false);
             scorePanelD.draw(e);
             e.Graphics.ResetTransform();
         }
