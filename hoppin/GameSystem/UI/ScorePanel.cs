f78fe9bc9902d43b648b8246dfc2392772d24d69
Ryosuke Onose <dev@onose.ws>
2016-04-27 16:35:03 +0900

    スコアボードに偏差値グラフを追加

--- hoppin/GameSystem/UI/ScorePanel.cs
+++ hoppin/GameSystem/UI/ScorePanel.cs
@@ -15,20 +15,23 @@ namespace hoppin.GameSystem.UI
         Color panelColor = new Color();
         String playerName = "Null Player";
         int score = 0;
+        double stdscore = 50;
         bool active = false;
 
         public ScorePanel() { }
-        public ScorePanel(String playerID_, Color panelColor_, String playerName_, int score_, bool active_) {
-            playerID = playerID_;
-            panelColor = panelColor_;
-            playerName = playerName_;
-            score = score_;
-            active = active_;
+        public ScorePanel(String playerID, Color panelColor, String playerName, int score, double stdscore, bool active) {
+            this.playerID = playerID;
+            this.panelColor = panelColor;
+            this.playerName = playerName;
+            this.score = score;
+            this.stdscore = stdscore;
+            this.active = active;
 
         }
         public void draw(PaintEventArgs e)
         {
             e.Graphics.FillRectangle(new SolidBrush(panelColor), 0, 0, 210, 82);
+            e.Graphics.FillRectangle(new SolidBrush(style.scoreInactiveColor), (stdscore > 0)?(int)((stdscore / 100.0) * 210):0, 0, (stdscore > 0)?220:0, 82);
             //e.Graphics.FillRectangle(new SolidBrush(Color.Aqua), 0, 0, 82, 82);
             SolidBrush idColor = new SolidBrush((active) ?style.scoreColor:style.scoreInactiveColor);
             e.Graphics.DrawString(playerID, new Font("Impact", 60), idColor, new PointF(-3, -8));
